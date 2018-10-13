349a746aedaafd4df56d994ef3eaba189dd4b868
Gautam Mokal <42087621+GautamSMok@users.noreply.github.com>
2018-10-13 19:39:37 +0530

    Assert that php examples.

--- OtherLanguages/php/test.php
+++ OtherLanguages/php/test.php
@@ -1,1 +1,36 @@
-//update this file
+<?php
+	require_once('SimpleAssertThat.php');
+	
+	Assert::that(
+	Method
+	::name('div')
+	::withParameters(1,2)
+	::inClass('Arithmatic')
+	,Throws::noException()
+	,'division is not working'
+	,'Hello Test');
+	
+
+Assert.that(someMethod(1,1,1,1,1),Is.equalTo(5));
+
+	class Arithmatic
+	{
+		
+		public function someMethod($a,$b,$c,$d,$e)
+		{
+			return $a+$b+$c+$d+$e;
+		}
+		
+		public function div($a,$b)
+		{
+			if($b==0)
+			{
+				throw new Exception('Division by zero');
+			}
+			else
+			{
+				throw new Exception('Division by zero');
+			}
+		}
+	}
+?>
